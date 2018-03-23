3612ce2f690874bbdd770ceee726510dd0692bea
858 Project s.r.o <info@858project.com>
2018-03-23 08:30:18 +0100

    Updating NET protocol

---
+++ 858project/858project.Net/FrameItemUInt16.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Project858;
+
+namespace Project858.Net
+{
+    /// <summary>
+    /// Frame item with Int16 value
+    /// </summary>
+    public sealed class FrameItemUInt16 : FrameItemBase<UInt16>
+    {
+        #region - Constructors -
+        /// <summary>
+        /// Initialize this class
+        /// </summary>
+        /// <param name="address">Item address</param>
+        /// <param name="value">Value</param>
+        public FrameItemUInt16(UInt32 address, UInt16 value)
+            : base(address, value)
+        {
+ 
+        }
+        /// <summary>
+        /// Initialize this class
+        /// </summary>
+        /// <param name="address">Item address</param>
+        /// <param name="data">Byte array</param>
+        public FrameItemUInt16(UInt32 address, Byte[] data)
+            : base(address, data)
+        {
+ 
+        }
+        #endregion
+
+        #region - Properties -
+        /// <summary>
+        /// Item type
+        /// </summary>
+        public override FrameItemTypes ItemType { get { return FrameItemTypes.UInt16; } }
+        #endregion
+
+        #region - Public Methods -
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            return String.Format("[Int16] : 0x{0:X4} = {1}", this.Address, this.Value);
+        }
+        #endregion
+
+        #region - Private Methods -
+        /// <summary>
+        /// This function parse value from byt array
+        /// </summary>
+        /// <param name="data">Byte array to parse</param>
+        /// <returns>Value</returns>
+        protected override UInt16 InternalParseValue(Byte[] data)
+        {
+            UInt16 result = (UInt16)(data[1] << 8 | data[0]);
+            return result;
+        }
+        /// <summary>
+        /// This function parse byt array from value
+        /// </summary>
+        /// <param name="value">Value to parse</param>
+        /// <returns>Byte array</returns>
+        protected override Byte[] InternalParseFromValue(UInt16 value)
+        {
+            Byte[] result = new Byte[2];
+            result[0] = (Byte)value;
+            result[1] = (Byte)(value >> 8);
+            return result;
+        }
+        #endregion
+    }
+}
